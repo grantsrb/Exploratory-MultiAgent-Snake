986a64d6f017b60bdf58c68d78b0a794fe70f7d0
Satchel Grant <grantsrb@gmail.com>
2018-02-16 12:02:48 -0800

    remove some unecessary stuff

--- dense_model.py
+++ dense_model.py
@@ -35,13 +35,12 @@ class Model(nn.Module):
 
     def forward(self, x):
         fx = x.data[:,0,:] - .5*x.data[:,1,:]
-        if self.view_net_input:
-            if self.viewer is None and x.shape[0] == 1:
-                self.viewer  = plt.imshow(fx.numpy().reshape(self.view_shape))
-            elif x.shape[0] == 1:
-                self.viewer.set_data(fx.numpy().reshape(self.view_shape))
-                plt.pause(0.5)
-                plt.draw()
+        #if self.viewer is None and x.shape[0] == 2:
+        #        self.viewer  = plt.imshow(fx[0].numpy().reshape(self.view_shape))
+        #elif x.shape[0] == 2:
+        #    self.viewer.set_data(fx[0].numpy().reshape(self.view_shape))
+        #    plt.pause(0.5)
+        #    plt.draw()
 
         fx = F.relu(self.entry(Variable(fx)))
         fx = self.bnorm1(fx)
@@ -69,18 +68,12 @@ class Model(nn.Module):
             p.requires_grad = grad_on
 
     @staticmethod
-    def preprocess(pic, env_type='snake-v0'):
-        if env_type == "Pong-v0":
-            pic = pic[35:195] # crop
-            pic = pic[::2,::2,0] # downsample by factor of 2
-            pic[pic == 144] = 0 # erase background (background type 1)
-            pic[pic == 109] = 0 # erase background (background type 2)
-            pic[pic != 0] = 1 # everything else (paddles, ball) just set to 1
-        elif env_type == "snake-v0":
-            new_pic = np.zeros(pic.shape[:2],dtype=np.float32)
-            new_pic[:,:][pic[:,:,0]==1] = 1
-            new_pic[:,:][pic[:,:,0]==255] = 1.5
-            new_pic[:,:][pic[:,:,1]==255] = 0
-            new_pic[:,:][pic[:,:,2]==255] = .33
-            pic = new_pic
+    def preprocess(pic, head_color):
+        new_pic = np.zeros(pic.shape[:2],dtype=np.float32)
+        new_pic[:,:][pic[:,:,0]==1] = 1
+        new_pic[:,:][pic[:,:,0]==255] = -1.25
+        new_pic[:,:][pic[:,:,1]==head_color[1]] = 1.25
+        new_pic[:,:][pic[:,:,1]==255] = 0
+        new_pic[:,:][pic[:,:,2]==255] = .33
+        pic = new_pic
         return pic.ravel()[None]
